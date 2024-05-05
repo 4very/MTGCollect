cae0bcadb165673d5fdfe752396f4fedf7747e87
Oksana <oksanaoeser@gmail.com>
2024-05-05 19:57:40 -0400

    checking my logic first
    dont wanna code for hours just to find out i made an oopsie early on

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <script setup>
-import Card from '../components/card.vue'
-import Pagination from '../components/Pagination.vue'
+import {
+ // PaginationEllipsis,
+ // PaginationFirst,
+  //PaginationLast,
+  //PaginationList,
+  //PaginationListItem,
+  PaginationNext,
+ // PaginationPrev,
+  //PaginationRoot,
+} from 'radix-vue'
+import {Card} from '../components/card.vue'
+import {Pagination} from '../components/Pagination.vue'
+
+const next = PaginationNext(['click'])
+console.log (next.click)
 </script>
 
 <template>
